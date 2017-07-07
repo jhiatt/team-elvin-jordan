5261460225caa9c48b500124768387b7c406b2e5
Elvin Herrera <nivle94@hotmail.com>
2017-07-06 20:09:52 -0400

    seed dumped model/ resourced pictures/ changed show action on picture controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Picture.create!([
+  {url: "http://lh4.ggpht.com/-Qk20NcwWeaA/UqWOzMCLDoI/AAAAAAAASCE/hqV8HtQJs9k/s1600/igloo_village_kakslauttanen1.jpg", title: "funky igloo", awesome: true},
+  {url: "http://i.imgur.com/2uPuX9G.jpg", title: "Rocket leaving atmosphere", awesome: true},
+  {url: "http://centurycycles.com/merchant/91/images/site/giant_defy_composite_3_compact_12_z.jpg", title: "Regular Giant Bike", awesome: false},
+  {url: "http://s.hswstatic.com/gif/eye-of-tornado-1.jpg", title: "Scary 'Nado", awesome: false}
+])
